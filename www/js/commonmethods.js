218f4a35269519589b615df8443b911cf84a3022
moni pandey <pandeymoni08@gmail.com>
2016-05-11 21:53:36 -0700

    12 may

--- www/js/commonmethods.js
+++ www/js/commonmethods.js
@@ -329,7 +329,7 @@ var cm = {
 };
 
 cm.parseInitializer();
-$(document).on("pageinit", "#loadTask,#AccommodationPage,#allotAccommodation,#allotCab,#cabPage,#cardfilter,#cardlistpage,#viewCard,#commentsPage,#friendsContactsPage,#contactsPage,#conversationPage,#eviewcard,#eventsPage,#eventsGuestPage,#faqPage,#favouritespages,#filterContactsPage,#filterlistpage,#galleryPage,#guest,#importContactPage,#managePage,#newGuestPage,#newHostPage,#newOSGuestPage,#newWeddingRegistration,#osguestRequestPage,#outstationGuestsPage,#pictureCommentsPage,#picturePage,#privacyPage,#pickContactPage,#sendToMe,#sendToself,#weddingDetailsEdit", function(event) {
+$(document).on("pageinit", "#loadTask, #AccommodationPage,#allotAccommodation,#allotCab,#cabPage,#cardfilter,#cardlistpage,#viewCard,#commentsPage,#friendsContactsPage,#contactsPage,#conversationPage,#eviewcard,#eventsPage,#eventsGuestPage,#faqPage,#favouritespages,#filterContactsPage,#filterlistpage,#galleryPage,#guest,#importContactPage,#managePage,#newGuestPage,#newHostPage,#newOSGuestPage,#newWeddingRegistration,#osguestRequestPage,#outstationGuestsPage,#pictureCommentsPage,#picturePage,#privacyPage,#pickContactPage,#sendToMe,#sendToself,#weddingDetailsEdit", function(event) {
 
     $("#shaadiMenu").on("panelopen", function(event, ui) {
         //setting overflow : hidden and binding "touchmove" with event which returns false
@@ -345,7 +345,7 @@ $(document).on("pageinit", "#loadTask,#AccommodationPage,#allotAccommodation,#al
         $("#shaadiMenu").panel("close");
         //return false;
     }
-    $(document).on("swiperight", "#budgetmod ,#breakup,#friendsListPage,#AccommodationPage,#allotAccommodation,#allotCab,#cabPage,#cardfilter,#cardlistpage,#viewCard,#commentsPage,#friendsContactsPage,#contactsPage,#conversationPage,#eviewcard,#eventsPage,#eventsGuestPage,#faqPage,#favouritespages,#filterContactsPage,#filterlistpage,#galleryPage,#guest,#importContactPage,#managePage,#newGuestPage,#newHostPage,#newOSGuestPage,#newWeddingRegistration,#osguestRequestPage,#outstationGuestsPage,#pictureCommentsPage,#picturePage,#privacyPage,#pickContactPage,#sendToMe,#sendToself,#weddingDetailsEdit", function(e) {
+    $(document).on("swiperight", "#budgetmod,#conversationPage,#groupconv, #friendsListPage,#breakup,#friendsListPage,#AccommodationPage,#allotAccommodation,#allotCab,#cabPage,#cardfilter,#cardlistpage,#viewCard,#commentsPage,#friendsContactsPage,#contactsPage,#conversationPage,#eviewcard,#eventsPage,#eventsGuestPage,#faqPage,#favouritespages,#filterContactsPage,#filterlistpage,#galleryPage,#guest,#importContactPage,#managePage,#newGuestPage,#newHostPage,#newOSGuestPage,#newWeddingRegistration,#osguestRequestPage,#outstationGuestsPage,#pictureCommentsPage,#picturePage,#privacyPage,#pickContactPage,#sendToMe,#sendToself,#weddingDetailsEdit", function(e) {
         // We check if there is no open panel on the page because otherwise
         // a swipe to close the left panel would also open the right panel (and v.v.).
         // We do this by checking the data that the framework stores on the page element (panel: open).
